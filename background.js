72c74d080600a051aca6127fd20c008cfaae3fcb
linkek <linusk@webqon.com>
2023-10-07 21:18:00 +0100

    cleanup

--- background.js
+++ background.js
@@ -2,20 +2,20 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.action === "openKeyboard") {
             // Send open keyboard request
-            fetch('http://localhost:3000/openKeyboard', {
+            fetch('http://localhost:7721/openKeyboard', {
                 method: 'POST'
             });
         }
 
         if (request.action === "closeKeyboard") {
             // Send close keyboard request
-            fetch('http://localhost:3000/closeKeyboard', {
+            fetch('http://localhost:7721/closeKeyboard', {
                 method: 'POST'
             });
         }
 
         if (request.action === "youClickedOn") {
-            fetch('http://localhost:3000/youClickedOn', {
+            fetch('http://localhost:7721/youClickedOn', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { exec } = require('child_process');
 
 const app = express();
-const PORT = 3000;
+const PORT = 7721;
 
 app.use(express.json());
 
